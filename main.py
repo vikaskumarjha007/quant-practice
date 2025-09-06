b62de9dc04f7c89c350fa816a7371fc80018c35f
Vikas Jha <vikasjha@192.168.1.4>
2025-09-06 23:51:33 +0530

    First program: numbers sum, difference, product

---
+++ main.py
@@ -0,0 +1,17 @@
+# main.py - first interactive program
+
+def numbers():
+    number1 = int(input("Enter first number: "))
+    number2 = int(input("Enter second number: "))
+
+    total = number1 + number2
+    print(f"The sum of {number1} and {number2} is {total}") 
+
+    difference = number1 - number2
+    print(f"The difference of {number1} and {number2} is {difference}")
+
+    product = number1 * number2
+    print(f"The product of {number1} and {number2} is {product}")
+
+if __name__ == "__main__":
+    numbers()
